1749a1ad543b4698d284016aac7f765c8b37e0d8
Catargo <cobauper@gmail.com>
2024-06-28 16:22:08 +0200

    fixed exports for the package

--- src/input-actions/index.ts
+++ src/input-actions/index.ts
@@ -2,11 +2,5 @@ export * from "./input_map";
 export * from "./input_key_code";
 export * from "./actions";
 
-export * from "./input_settings/input_settings";
-export * from "./input_manager/input_manager";
-
-export * from "./input_manager/input_event_action";
-import { default as InputEventClass } from "./input_manager/input_event";
-export type InputEvent = typeof InputEventClass;
-import { default as SubscriptionClass } from "./input_manager/input_broad_caster/subscription";
-export type Subscription = typeof SubscriptionClass;
+export * from "./input_settings";
+export * from "./input_manager";
---
+++ src/input-actions/input_manager/index.ts
@@ -0,0 +1,5 @@
+export * from "./input_manager";
+export { default as InputEventAction } from "./input_event_action";
+
+export type { default as Subscription } from "./input_broad_caster/subscription";
+export type { default as InputEvent } from "./input_event";
